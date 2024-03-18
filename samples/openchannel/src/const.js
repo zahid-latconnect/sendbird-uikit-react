98a393ffea7a055cd4adcc83fef9b474e43e1a9d
zahid-latconnect <123043805+zahid-latconnect@users.noreply.github.com>
2024-03-18 12:44:50 +0800

    test

--- samples/openchannel/src/const.js
+++ samples/openchannel/src/const.js
@@ -1,9 +1,9 @@
 // put your own APP_ID here
 // get your app_id -> https://dashboard.sendbird.com/auth/signin
-export const APP_ID = "";
+export const APP_ID = 'B80C8A97-4F63-4C06-BE35-11B8BA33C8E6';
 // set your own USER_ID and NICKNAME
-export const USER_ID = "sendbirdian-200720";
-export const NICKNAME = "Sendbirdian84";
+export const USER_ID = '970107335175';
+export const NICKNAME = 'zahid';
 
 export default {
   APP_ID,
